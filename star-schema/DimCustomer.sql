0c6bd03c85c6c238415d8cef1744f51a7a2f52ef
Kiran <83135224+kalagonikiran@users.noreply.github.com>
2024-03-17 19:31:59 +0530

    Create DimCustomer.sql

---
+++ star-schema/DimCustomer.sql
@@ -0,0 +1,10 @@
+INSERT INTO "DimCustomer"(customerid,category,country,industry)
+VALUES
+(1,'Individual','Indonesia','Engineering'),
+(614,'Individual','United States','Product Management'),
+(615,'Individual','China','Services'),
+(616,'Individual','Russia','Accounting'),
+(617,'Individual','Chile','Business Development'),
+(618,'Individual','Nicaragua','Human Resources'),
+(41,'Company','Brazil','Marketing'),
+(619,'Individual','Russia','Business Development')
