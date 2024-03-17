d7e479fb57c537ae3a64fc05052c5bbef6fe8517
Kiran <83135224+kalagonikiran@users.noreply.github.com>
2024-03-17 19:31:10 +0530

    Create DimMonth.sql

---
+++ star-schema/DimMonth.sql
@@ -0,0 +1,11 @@
+''' inserting values'''
+  INSERT INTO "DimMonth"
+(monthid,year,month,monthname,quarter,quartername)
+VALUES
+(20091, 2009, 1, 'Janauary', 1, 'Q1'),
+(200910, 2009, 10, 'October', 4, 'Q4'),
+(200911, 2009, 11, 'November', 4, 'Q4'),
+(200912, 2009, 12, 'December', 4, 'Q4'),
+(20092, 2009, 2, 'February', 1, 'Q1'),
+(20093, 2009, 3, 'March', 1, 'Q1'),
+(20094, 2009, 4, 'April', 2, 'Q2'),
